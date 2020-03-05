fa252b812ff429526f8e936252cb52bb7fd922f2
Nian LinuxOS <azharnian@gmail.com>
2020-03-05 09:54:14 +0700

    Perbaikan Compatible CI3

--- application/third_party/MX/Router.php
+++ application/third_party/MX/Router.php
@@ -239,18 +239,7 @@ class MX_Router extends CI_Router
 			}
 		}
 	}
-
-	// public function set_class($class)
-	// {
-	// 	$suffix = $this->config->item('controller_suffix');
-
-	// 	if (strpos($class, $suffix) === FALSE)
-	// 	{
-	// 		$class .= $suffix;
-	// 	}
-
-	// 	parent::set_class($class);
-	// }
+	
 public function set_class($class)
 	{
 		$suffix = $this->config->item('controller_suffix');
